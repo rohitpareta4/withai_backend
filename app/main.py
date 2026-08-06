7502bc7ba42b7610a0882aa594cad753ea39132f
rohitpareta4 <rockk7x@gmail.com>
2026-08-06 22:39:08 +0530

    fix url

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://with-ai-1859.vercel.app"],
+    allow_origins=["http://localhost:3000","https://with-ai-x1.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
